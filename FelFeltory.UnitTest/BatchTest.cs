2f5f58179f7f9ed80f655c72df29c24362073cdd
Elia Noris <enoris@unifysquare.com>
2020-08-04 17:40:22 +0200

    Added: Unit Tests Project
    Added: Test Coverage for Batch

---
+++ FelFeltory.UnitTest/BatchTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FelFeltory.Models;
+
+namespace FelFeltory.UnitTest
+{
+    [TestClass]
+    public class BatchTest
+    {
+        [TestMethod]
+        public void VerifyIsFresh()
+        {
+            Batch b = new Batch();
+            b.Expiration = DateTime.UtcNow.AddDays(10);
+
+            Freshness f = b.Freshness;
+
+            Assert.IsTrue(f == Freshness.Fresh);
+        }
+
+        public void VerifyExpiresToday()
+        {
+            Batch b = new Batch();
+            b.Expiration = DateTime.UtcNow.AddHours(10);
+
+            Freshness f = b.Freshness;
+
+            Assert.IsTrue(f == Freshness.ExpiringToday);
+        }
+
+        public void VerifyExpired()
+        {
+            Batch b = new Batch();
+            b.Expiration = DateTime.UtcNow.AddDays(-1);
+
+            Freshness f = b.Freshness;
+
+            Assert.IsTrue(f == Freshness.Expired);
+        }
+    }
+}
---
+++ FelFeltory.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FelFeltory.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Unify Square Inc.")]
+[assembly: AssemblyProduct("FelFeltory.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Unify Square Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("905c27a3-a414-41e7-a763-21a1722e1830")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
